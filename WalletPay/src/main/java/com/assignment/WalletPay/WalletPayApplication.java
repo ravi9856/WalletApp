7f04fdbd56aa614205ce2f8c734328c878116b58
Ravindra Kumar <ravi.loveyouall@gmail.com>
2020-08-18 15:09:24 +0530

    added Project base structure from Spring.io

---
+++ WalletPay/src/main/java/com/assignment/WalletPay/WalletPayApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.WalletPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletPayApplication.class, args);
+	}
+
+}
---
+++ WalletPay/src/test/java/com/assignment/WalletPay/WalletPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.WalletPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
